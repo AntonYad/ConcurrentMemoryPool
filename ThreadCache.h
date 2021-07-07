15683832227fceeff732b7a0e226380e14c459f4
AntonYad <651299847@qq.com>
2021-07-07 23:35:38 +0800

    ThreadCache.h

---
+++ ThreadCache.h
@@ -0,0 +1,23 @@
+#ifndef __THREAD_CACHE_H__
+#define __THREAD_CACHE_H__
+
+#include "Common.h"
+
+class ThreadCache
+{
+public:
+	// 申请和释放内存对象
+	void* Allocate(size_t size);
+	void Deallocate(void* ptr, size_t size);
+
+	// 从中心缓存获取对象
+	void* FetchFromCentralCache(size_t index, size_t size);
+	// 释放对象时，链表过长时，回收内存回到中心堆
+	void ListTooLong(FreeList* list, size_t size);
+private:
+	FreeList _freeList[NLISTS];	   // 自由链表
+};
+
+static __declspec(thread) ThreadCache* tls_threadcache = nullptr;
+
+#endif // __THREAD_CACHE_H__
